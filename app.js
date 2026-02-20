3ec335dc3de1f56691065e2af3162217ea251323
tzgomes <thiago.zgomes@gmail.com>
2026-02-20 17:52:41 -0300

    adicionando console.log

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("rodando o sistema de gerenciamento de jogos")
+// atualizando o código da aula
+console.log("aplicaçao conectando ao BD")
+console.log("O Zanon fez esse commit")
+console.log("Enviando para o Gitthub")
